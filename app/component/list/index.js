9d0ce9957fe2dce880f5c010178f1e50292c9810
seraphimrose <jifeifan@baixing.com>
2016-04-22 17:32:42 +0800

    add card UI

--- app/component/list/index.js
+++ app/component/list/index.js
@@ -1,29 +1,79 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Icon from 'antd/lib/icon'
+import Button from 'antd/lib/button'
 
 import Card from 'component/card'
 import style from './list.css'
 
-export default (props) => {
-	const { list } = props
-	return (
-		<div className={style.list}>
-			<div className={style.title}>
-				<h3>{list.get('title')}</h3>
-			</div>
-			<div className={style.content}>
-				{list.get('card') &&
-					list.get('card').map(value => (
-					<Card
-						{...props}
-						key={value}
-						card={props.card.get(value)}
-					/>
-				))}
-				<div className={style.new}>
-					<Icon className={style.icon} type="plus" />Add a card...
+class AddCard extends Component {
+
+	constructor(props) {
+		super(props)
+	}
+
+	componentDidMount() {
+		this.refs.myInput.focus()
+	}
+
+	render() {
+		const { cancel } = this.props
+		return (
+			<div className={style.add}>
+				<input ref="myInput" type="text" placeholder="title"/>
+				<div className={style.control}>
+					<Button onClick={cancel}
+							type="primary" size="small">Add</Button>
+					<Button onClick={cancel}
+							type="ghost" size="small">Cancel</Button>
 				</div>
 			</div>
-		</div>
-	)
+		)
+	}
+}
+
+export default class List extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {isAdding: false}
+		this.add = this.add.bind(this)
+		this.cancel = this.cancel.bind(this)
+	}
+
+	add() {
+		this.setState({isAdding: true})
+	}
+
+	cancel() {
+		this.setState({isAdding: false})
+	}
+
+	render() {
+		const { list, card } = this.props
+		return (
+			<div className={style.list}>
+				<div className={style.title}>
+					<h3>{list.get('title')}</h3>
+				</div>
+				<div className={style.content}>
+					{list.get('card') &&
+					list.get('card').map(value => (
+						<Card
+							{...this.props}
+							key={value}
+							card={card.get(value)}
+						/>
+					))}
+					{this.state.isAdding ? (
+						<AddCard
+							cancel={this.cancel}
+						/>
+					) : (
+						<div onClick={this.add} className={style.new}>
+							<Icon className={style.icon} type="plus" />Add a card...
+						</div>
+					)}
+				</div>
+			</div>
+		)
+	}
 }
